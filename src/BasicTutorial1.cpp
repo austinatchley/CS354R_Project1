7b5cd009f6281d526f019c9ff4fc185449784b71
Austin Atchley <austinatchley@gmail.com>
2018-09-14 14:35:11 -0500

    Modified namespace scope, Send TestEvent on Space press

--- src/BasicTutorial1.cpp
+++ src/BasicTutorial1.cpp
@@ -27,167 +27,140 @@ THE SOFTWARE
 -------------------------------------------------------------------------*/
 
 //! [fullsource]
+#include "BasicTutorial1.h"
 
 #include <Ogre.h>
 #include <OgreApplicationContext.h>
 #include <OgreInput.h>
 #include <OgreRTShaderSystem.h>
 #include <OgreApplicationContext.h>
-#include <iostream>
-
-#include "EventManager.h"
-#include "EntityManager.h"
-#include "ComponentManager.h"
-#include "GameObject.h"
-#include "Ball.h"
 
 using namespace Ogre;
 using namespace OgreBites;
 
-class BasicTutorial1
-        : public ApplicationContext
-        , public InputListener
+namespace Game
 {
-public:
-    BasicTutorial1();
-    virtual ~BasicTutorial1() {}
-
-    void setup();
-    bool keyPressed(const KeyboardEvent& evt);
-};
-
-
-BasicTutorial1::BasicTutorial1()
-    : ApplicationContext("OgreTutorialApp")
-{
-}
-
-
-struct TestEvent
-{
-    int i;
-    char c;
-};
-
-class TestEventSubscriber
-    : public ECS::EventSubscriber<TestEvent>
-{
-public:
-    virtual ~TestEventSubscriber() {}
-
-    virtual void receive(ECS::EventManager* eventManager, const TestEvent& event) override
+    BasicTutorial1::BasicTutorial1()
+        : ApplicationContext("OgreTutorialApp")
     {
-        std::cout << "TestEvent was emitted" << std::endl;
     }
-};
 
-void BasicTutorial1::setup()
-{
-    // do not forget to call the base first
-    ApplicationContext::setup();
-    addInputListener(this);
-
-    // get a pointer to the already created root
-    Root* root = getRoot();
-    SceneManager* scnMgr = root->createSceneManager();
-
-    // register our scene with the RTSS
-    RTShader::ShaderGenerator* shadergen = RTShader::ShaderGenerator::getSingletonPtr();
-    shadergen->addSceneManager(scnMgr);
-
-    // -- tutorial section start --
-    //! [turnlights]
-    scnMgr->setAmbientLight(ColourValue(0.5, 0.5, 0.5));
-    //! [turnlights]
-
-    //! [newlight]
-    Light* light = scnMgr->createLight("MainLight");
-    SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
-    lightNode->attachObject(light);
-    //! [newlight]
-
-    //! [lightpos]
-    lightNode->setPosition(20, 80, 50);
-    //! [lightpos]
-
-    //! [camera]
-    SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
-
-    // create the camera
-    Camera* cam = scnMgr->createCamera("myCam");
-    cam->setNearClipDistance(5); // specific to this sample
-    cam->setAutoAspectRatio(true);
-    camNode->attachObject(cam);
-    camNode->setPosition(0, 0, 140);
-
-    // and tell it to render into the main window
-    getRenderWindow()->addViewport(cam);
-    //! [camera]
-
-    //! [entity1]
-    Entity* ogreEntity = scnMgr->createEntity("ogrehead.mesh");
-    //! [entity1]
-
-    //! [entity1node]
-    SceneNode* ogreNode = scnMgr->getRootSceneNode()->createChildSceneNode();
-    //! [entity1node]
-
-    //! [entity1nodeattach]
-    ogreNode->attachObject(ogreEntity);
-    //! [entity1nodeattach]
-
-    //! [cameramove]
-    camNode->setPosition(0, 47, 222);
-    //! [cameramove]
-
-    //! [entity2]
-    Entity* ogreEntity2 = scnMgr->createEntity("ogrehead.mesh");
-    SceneNode* ogreNode2 = scnMgr->getRootSceneNode()->createChildSceneNode(Vector3(84, 48, 0));
-    ogreNode2->attachObject(ogreEntity2);
-    //! [entity2]
-
-    //! [entity3]
-    Entity* ogreEntity3 = scnMgr->createEntity("ogrehead.mesh");
-    SceneNode* ogreNode3 = scnMgr->getRootSceneNode()->createChildSceneNode();
-    ogreNode3->setPosition(0, 104, 0);
-    ogreNode3->setScale(2, 1.2, 1);
-    ogreNode3->attachObject(ogreEntity3);
-    //! [entity3]
-
-    //! [entity4]
-    Entity* ogreEntity4 = scnMgr->createEntity("ogrehead.mesh");
-    SceneNode* ogreNode4 = scnMgr->getRootSceneNode()->createChildSceneNode();
-    ogreNode4->setPosition(-84, 48, 0);
-    ogreNode4->roll(Degree(-90));
-    ogreNode4->attachObject(ogreEntity4);
-    //! [entity4]
-
-    // -- tutorial section end --
-    
-    ECS::EventManager* eventManager = new ECS::EventManager(std::allocator<void>());
-
-    TestEventSubscriber* sub = new TestEventSubscriber();
-    eventManager->connect<TestEvent>(sub);
-
-    //eventManager->event<TestEvent>({ 1, 'a' });
-}
-
-
-bool BasicTutorial1::keyPressed(const KeyboardEvent& evt)
-{
-    if (evt.keysym.sym == SDLK_ESCAPE)
+    void BasicTutorial1::setup()
     {
-        getRoot()->queueEndRendering();
-    }
-    return true;
-}
+        // do not forget to call the base first
+        ApplicationContext::setup();
+        addInputListener(this);
 
+        // get a pointer to the already created root
+        Root* root = getRoot();
+        SceneManager* scnMgr = root->createSceneManager();
+
+        // register our scene with the RTSS
+        RTShader::ShaderGenerator* shadergen = RTShader::ShaderGenerator::getSingletonPtr();
+        shadergen->addSceneManager(scnMgr);
+
+        // -- tutorial section start --
+        //! [turnlights]
+        scnMgr->setAmbientLight(ColourValue(0.5, 0.5, 0.5));
+        //! [turnlights]
+
+        //! [newlight]
+        Light* light = scnMgr->createLight("MainLight");
+        SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+        lightNode->attachObject(light);
+        //! [newlight]
+
+        //! [lightpos]
+        lightNode->setPosition(20, 80, 50);
+        //! [lightpos]
+
+        //! [camera]
+        SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+
+        // create the camera
+        Camera* cam = scnMgr->createCamera("myCam");
+        cam->setNearClipDistance(5); // specific to this sample
+        cam->setAutoAspectRatio(true);
+        camNode->attachObject(cam);
+        camNode->setPosition(0, 0, 140);
+
+        // and tell it to render into the main window
+        getRenderWindow()->addViewport(cam);
+        //! [camera]
+
+        //! [entity1]
+        Entity* ogreEntity = scnMgr->createEntity("ogrehead.mesh");
+        //! [entity1]
+
+        //! [entity1node]
+        SceneNode* ogreNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+        //! [entity1node]
+
+        //! [entity1nodeattach]
+        ogreNode->attachObject(ogreEntity);
+        //! [entity1nodeattach]
+
+        //! [cameramove]
+        camNode->setPosition(0, 47, 222);
+        //! [cameramove]
+
+        //! [entity2]
+        Entity* ogreEntity2 = scnMgr->createEntity("ogrehead.mesh");
+        SceneNode* ogreNode2 = scnMgr->getRootSceneNode()->createChildSceneNode(Vector3(84, 48, 0));
+        ogreNode2->attachObject(ogreEntity2);
+        //! [entity2]
+
+        //! [entity3]
+        Entity* ogreEntity3 = scnMgr->createEntity("ogrehead.mesh");
+        SceneNode* ogreNode3 = scnMgr->getRootSceneNode()->createChildSceneNode();
+        ogreNode3->setPosition(0, 104, 0);
+        ogreNode3->setScale(2, 1.2, 1);
+        ogreNode3->attachObject(ogreEntity3);
+        //! [entity3]
+
+        //! [entity4]
+        Entity* ogreEntity4 = scnMgr->createEntity("ogrehead.mesh");
+        SceneNode* ogreNode4 = scnMgr->getRootSceneNode()->createChildSceneNode();
+        ogreNode4->setPosition(-84, 48, 0);
+        ogreNode4->roll(Degree(-90));
+        ogreNode4->attachObject(ogreEntity4);
+        //! [entity4]
+
+        // -- tutorial section end --
+        
+        mEventManager = new ECS::EventManager(std::allocator<void>());
+
+        TestEventSubscriber* sub = new TestEventSubscriber();
+        mEventManager->connect<TestEvent>(sub);
+
+        mEventManager->event<TestEvent>({ 1, 'a' });
+    }
+
+    static int counter = 0;
+    bool BasicTutorial1::keyPressed(const KeyboardEvent& evt)
+    {
+        switch (evt.keysym.sym)
+        {
+        case SDLK_ESCAPE:
+            getRoot()->queueEndRendering();
+            break;
+        case SDLK_SPACE:
+            mEventManager->event<TestEvent>({ ++counter, static_cast<char>('a' + (counter % 26)) });
+            break;
+        default:
+            break;
+        }
+
+        return true;
+    }
+    //! [fullsource]
+} // namespace Game
 
 int main(int argc, char **argv)
 {
     try
     {
-    	BasicTutorial1 app;
+        Game::BasicTutorial1 app;
         app.initApp();
         app.getRoot()->startRendering();
         app.closeApp();
@@ -201,4 +174,4 @@ int main(int argc, char **argv)
     return 0;
 }
 
-//! [fullsource]
+
---
+++ src/BasicTutorial1.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <Ogre.h>
+#include <OgreApplicationContext.h>
+#include <OgreInput.h>
+#include <OgreRTShaderSystem.h>
+#include <OgreApplicationContext.h>
+
+#include <iostream>
+
+#include "EventManager.h"
+#include "EntityManager.h"
+#include "ComponentManager.h"
+#include "GameObject.h"
+#include "Ball.h"
+
+using namespace Ogre;
+using namespace OgreBites;
+
+namespace Game
+{
+    class BasicTutorial1
+            : public ApplicationContext
+            , public InputListener
+    {
+    public:
+        BasicTutorial1();
+        virtual ~BasicTutorial1() {}
+
+        void setup();
+        bool keyPressed(const KeyboardEvent& evt);
+
+    private:
+        ECS::EventManager* mEventManager;
+    };
+
+    struct TestEvent
+    {
+        int i;
+        char c;
+    };
+
+    class TestEventSubscriber
+        : public ECS::EventSubscriber<TestEvent>
+    {
+    public:
+        virtual ~TestEventSubscriber() {}
+
+        virtual void receive(ECS::EventManager* eventManager, const TestEvent& event) override
+        {
+            std::cout << "TestEvent was emitted with i = " << event.i << ", c = " << event.c << std::endl;
+        }
+    };
+} //namespace Game
