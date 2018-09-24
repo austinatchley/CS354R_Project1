1b290b98377d232d2fd5adc490dd178b0d0a0078
Austin Atchley <austinatchley@gmail.com>
2018-09-24 16:08:53 -0500

    Extracted executable

--- build/CMakeFiles/Project1.dir/DependInfo.cmake
+++ build/CMakeFiles/Project1.dir/DependInfo.cmake
@@ -4,25 +4,25 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/austin/Downloads/Ogre1-11_Bootstrap/src/Ball.cpp" "/home/austin/Downloads/Ogre1-11_Bootstrap/build/CMakeFiles/Project1.dir/src/Ball.cpp.o"
-  "/home/austin/Downloads/Ogre1-11_Bootstrap/src/BasicTutorial1.cpp" "/home/austin/Downloads/Ogre1-11_Bootstrap/build/CMakeFiles/Project1.dir/src/BasicTutorial1.cpp.o"
-  "/home/austin/Downloads/Ogre1-11_Bootstrap/src/EventManager.cpp" "/home/austin/Downloads/Ogre1-11_Bootstrap/build/CMakeFiles/Project1.dir/src/EventManager.cpp.o"
-  "/home/austin/Downloads/Ogre1-11_Bootstrap/src/GameObject.cpp" "/home/austin/Downloads/Ogre1-11_Bootstrap/build/CMakeFiles/Project1.dir/src/GameObject.cpp.o"
+  "/v/filer5b/v38q001/austina/GameTech/Project1/src/Ball.cpp" "/v/filer5b/v38q001/austina/GameTech/Project1/build/CMakeFiles/Project1.dir/src/Ball.cpp.o"
+  "/v/filer5b/v38q001/austina/GameTech/Project1/src/Game.cpp" "/v/filer5b/v38q001/austina/GameTech/Project1/build/CMakeFiles/Project1.dir/src/Game.cpp.o"
+  "/v/filer5b/v38q001/austina/GameTech/Project1/src/SoundManager.cpp" "/v/filer5b/v38q001/austina/GameTech/Project1/build/CMakeFiles/Project1.dir/src/SoundManager.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include/OGRE/Bites"
-  "/usr/local/include/OGRE"
-  "/usr/local/include/OGRE/Overlay"
-  "/usr/local/include/OGRE/RTShaderSystem"
-  "/usr/local/include/OGRE/HLMS"
-  "/usr/local/include/OGRE/MeshLodGenerator"
-  "/usr/local/include/OGRE/Paging"
-  "/usr/local/include/OGRE/Property"
-  "/usr/local/include/OGRE/Terrain"
-  "/usr/local/include/OGRE/Volume"
+  "../include"
+  "/lusr/opt/ogre-1.11/include/OGRE/Bites"
+  "/lusr/opt/ogre-1.11/include/OGRE"
+  "/lusr/opt/ogre-1.11/include/OGRE/Overlay"
+  "/lusr/opt/ogre-1.11/include/OGRE/RTShaderSystem"
+  "/lusr/opt/ogre-1.11/include/OGRE/HLMS"
+  "/lusr/opt/ogre-1.11/include/OGRE/MeshLodGenerator"
+  "/lusr/opt/ogre-1.11/include/OGRE/Paging"
+  "/lusr/opt/ogre-1.11/include/OGRE/Property"
+  "/lusr/opt/ogre-1.11/include/OGRE/Terrain"
+  "/lusr/opt/ogre-1.11/include/OGRE/Volume"
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/Project1.dir/cmake_clean.cmake
+++ build/CMakeFiles/Project1.dir/cmake_clean.cmake
@@ -1,8 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Project1.dir/src/BasicTutorial1.cpp.o"
-  "CMakeFiles/Project1.dir/src/GameObject.cpp.o"
+  "CMakeFiles/Project1.dir/src/Game.cpp.o"
   "CMakeFiles/Project1.dir/src/Ball.cpp.o"
-  "CMakeFiles/Project1.dir/src/EventManager.cpp.o"
+  "CMakeFiles/Project1.dir/src/SoundManager.cpp.o"
   "Project1.pdb"
   "Project1"
 )
